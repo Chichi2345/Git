dc36edff6719f0cf68820231be0f005bb5ae8b11
Chichi2345 <167276110+Chichi2345@users.noreply.github.com>
2024-04-18 10:59:09 +0800

    Update trial.py

--- trial.py
+++ trial.py
@@ -1,6 +1,7 @@
+print("Welcome!")
 number_1 = int(input("Enter your first number: "))
 number_2 = int(input("Enter your second number:"))
-operation = input("Choose the operation (+, -, *): ")
+operation = input("Choose the operation (+, -, *, /): ")
 
 if operation == "+":
     print('{} + {} = '.format(number_1, number_2))
@@ -13,4 +14,7 @@ elif operation == "-":
 elif operation == "*":
     print('{} * {} = '.format(number_1, number_2))
     print(number_1 * number_2)
-    
+
+elif operation == "/":
+    print('{} / {} = '.format(number_1, number_2))
+    print(number_1 / number_2)
